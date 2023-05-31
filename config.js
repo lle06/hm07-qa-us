4959020e804a4cd7154a8c57bedcada5adb461bd
Linda Le <lindale06@gmail.com>
2023-05-31 17:38:20 -0600

    Sprint 7

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // eslint-disable-next-line no-undef
 module.exports = {
-    API_URL: " https://464930f5-45bd-4188-a814-a54ff5b92512.serverhub.practicum-services.com"
+    API_URL: "https://464930f5-45bd-4188-a814-a54ff5b92512.serverhub.practicum-services.com"
 }
