e752fe22646661db411544ed1e12d0023af555c2
Linda Le <lindale06@gmail.com>
2023-05-31 18:08:20 -0600

    modified: config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // eslint-disable-next-line no-undef
 module.exports = {
-    API_URL: "https://8ef3c64a-3918-4b86-b260-cc46e54bcb61.serverhub.practicum-services.com"
+    API_URL: "https://cee55068-a3ea-4333-99e2-c057d4be35c1.serverhub.practicum-services.com"
 }
