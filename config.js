e0298fbd2f4b8c6b60ed3b0d77e204c88d8855b9
Linda Le <lindale06@gmail.com>
2023-05-31 20:30:02 -0600

    Sprint 7

--- config.js
+++ config.js
@@ -1,3 +1,4 @@
 // eslint-disable-next-line no-undef
 module.exports = {
-    API_URL: "https://942e31fd-4b23-42b9-8e2f-932b43fc869f.serverhub.practicum-services.com"
+    API_URL: "https://cee55068-a3ea-4333-99e2-c057d4be35c1.serverhub.practicum-services.com"
+}
