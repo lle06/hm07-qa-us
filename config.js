7feed23fcf1bc9da4a61cc1f506edc1f25bdefb1
Linda Le <lindale06@gmail.com>
2023-09-11 15:33:55 -0600

    test commit

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // eslint-disable-next-line no-undef
 module.exports = {
-    API_URL: "https://fe4aaf04-e163-416d-b2d8-f38d603195eb.serverhub.practicum-services.com"
+    API_URL: "https://b0c00d48-77d1-425d-9afd-b405447c049d.serverhub.practicum-services.com"
 }
